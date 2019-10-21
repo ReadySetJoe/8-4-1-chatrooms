9a2a806ed991ea1e3db144a05b7f28b9d3e2633c
ReadySetJoe <wolfgangwallaceband@gmail.com>
2019-10-21 09:30:51 -0400

    heroku edits

--- conf/urls.py
+++ conf/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-if settings.DEBUG: # new
+if settings.DEBUG:  # new
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
